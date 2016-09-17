04d13c19d535cad9c465d4e3dd826789025f33f3
Theo Pack Test <tf.pack@gmail.com>
2016-09-17 15:50:41 +0200

    Add function to generate guid

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ var handleRequest = function(request, response) {
 };
 var www = http.createServer(handleRequest);
 www.listen(3000);
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
